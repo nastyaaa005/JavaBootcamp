6a6168facb3ed6da43bb66a2ca6e03f3762c3f22
Nallade <solvita.v@gmail.com>
2018-08-01 11:07:35 +0300

    Task 3

--- SEF/src/sef/ATestTask/FirstActivity/Student.java
+++ SEF/src/sef/ATestTask/FirstActivity/Student.java
@@ -3,5 +3,23 @@ package sef.ATestTask.FirstActivity;
 
 //TODO Implement class Student
 
-public class Student {
+public class Student extends Person {
+    public String university;
+
+    public Student() {
+        this.university = "Latvijas Universitate";
+    }
+
+    public Student(String university) {
+        this.university = university;
+    }
+
+    public String getUniversity() { return university;}
+
+    public void setUniversity(String university) { this.university = university; }
+
+    public static void main(String[] args) {
+        Student one = new Student();
+        System.out.println("My name is " +  one.firstName + " and I study at " + one.university + ".");
+    }
 }
