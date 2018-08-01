8f308e0edefcbb86a71c96c4db8e1adff644835e
Akaryst <artjoms.jermaks@gmail.com>
2018-08-01 10:01:22 +0300

    TestTask

--- SEF/src/sef/ATestTask/FirstActivity/Employee.java
+++ SEF/src/sef/ATestTask/FirstActivity/Employee.java
@@ -2,19 +2,69 @@ package sef.ATestTask.FirstActivity;
 
 public class Employee extends Person {
 
+    // Attributes
     private int empId;
     private String jobTitle;
     private String companyName;
     private double salary;
 
     //TODO 1 create different constructors
-    public Employee(){
 
+    // Employee's default constructor
+    public Employee(){
+        this.empId = 0;
+        this.jobTitle = "Unknown";
+        this.companyName = "Unknown";
+        this.salary = 0;
+    }
+
+    // Employee's parameterized constructor
+    public Employee(int empId, String jobTitle, String companyName, double salary) {
+        this.empId = empId;
+        this.jobTitle = jobTitle;
+        this.companyName = companyName;
+        this.salary = salary;
     }
 
     //TODO 2 add getters and setters
 
+    // getter/setter for int empId
+    public int getEmpId() {
+        return empId;
+    }
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
 
+    // getter/setter for String jobTitle
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 
+    // getter/setter for String companyName
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    // getter/setter for int empId
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Employee's introduction method
+    @Override
+    public void announce() {
+        System.out.println("My name is " + getFirstName() + getSecondName() + " and I am " + getAge() + " years old");
+        System.out.println("I work as " + getTitle() + " in "  + getCompany());
+    }
 
 }
--- SEF/src/sef/ATestTask/FirstActivity/Person.java
+++ SEF/src/sef/ATestTask/FirstActivity/Person.java
@@ -5,45 +5,52 @@ public class Person {
 
 //TODO 1 Implement Person Attributes
 
-	//Behavior - default constructor
-	public Person(){
+	//Attributes
+	private String firstName, secondName;
+	private int age;
+
+	// Person's default constructor
+	public Person() {
 		this.firstName="Unknown";
 		this.secondName = "Unknown";
 		this.age = 0;
 	}
 
-	//Behavior - parameterized constructor
-	public Person(String firstName, String secondName, int age){
+	// Person's parameterized constructor
+	public Person(String firstName, String secondName, int age) {
 		this.firstName = firstName;
 		this.secondName = secondName;
 		this.age = age;
 	}
 
-	// getter for String firstName
+	// getter/setter for String firstName
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	// getter for int age
+	// getter/setter for String secondName
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	// getter/setter for int age
 	public int getAge() {
 		return age;
 	}
-
-	// setter for int age
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-	public String getSecondName() {
-		return secondName;
+	// Person's introduction method
+	public void announce() {
+		System.out.println("My name is " + getFirstName() + getSecondName() + " and I am " + getAge() + " years old");
 	}
 
-	public void setSecondName(String secondName) {
-		this.secondName = secondName;
-	}
 }
 
