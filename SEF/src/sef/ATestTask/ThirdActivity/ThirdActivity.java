4dd224c5a5db2ee6f24a91dfb414ab484acab349
Student <ghjh>
2018-08-01 16:33:02 +0300

    Class Test Work

--- SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
+++ SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
@@ -5,7 +5,7 @@ package sef.ATestTask.ThirdActivity;
 
 public class ThirdActivity {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ThirdActivity ta = new ThirdActivity();
         //TODO handle exception on proper way
         ta.validateUser("Ivan");
@@ -14,27 +14,38 @@ public class ThirdActivity {
 
     void catchExeption() {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    }
-
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(arr[i]);
+        try {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(arr[i]);
+            }
+            System.out.println("This should get printed even if there is an exception");
+        }catch (Exception e){
+            System.out.println("Exception occured");
         }
-        System.out.println("This should get printed even if there is an exception");
 
-    }
-
-    void validateUser(String name) {
+    void validateUser(String String name;
+        name) throws Exception {
         String[] validUsers = {"John", "Mike", "Missi", "Peacy"};
-        int flag = 0;
+        boolean flag = false;
+        for(int i=0;i<validUsers.length;i++){
+            if(validUsers[i].equals(name)){
+        }
         //TODO if name in a list -> set flag=1
         // if at the end flag=0 -> throw the exeption
+        if(flag== false) {
+            throw new Exception("User is not valid");
+        }
     }
 
     void catchMe(int num1, int num2)
     {
         //TODO Catch exeption
-            int result=num1/num2;
+        try {
+            int result = num1 / num2;
             System.out.println("The result is :" + result);
+        }catch (Exception e){
+            System.out.println(" Cannot divide by zero " + e);
+        }
 
             //TODO prints a message "Thank you for using this program." always
             System.out.println("Thank you for using this program.");
