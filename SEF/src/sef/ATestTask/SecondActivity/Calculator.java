e1106b5543ef474b841c895ed098786726561021
Nallade <solvita.v@gmail.com>
2018-08-01 15:38:58 +0300

    SecondActivity in process

---
+++ SEF/src/sef/ATestTask/SecondActivity/Calculator.java
@@ -0,0 +1,20 @@
+package sef.ATestTask.SecondActivity;
+
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double difference(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication (double a, double b) {
+        return a * b;
+    }
+
+    public double division (double a, double b) {
+        return a / b;
+    }
+}
--- SEF/src/sef/ATestTask/SecondActivity/SecondActivity.java
+++ SEF/src/sef/ATestTask/SecondActivity/SecondActivity.java
@@ -1,16 +1,28 @@
 package sef.ATestTask.SecondActivity;
+
+import java.util.Scanner;
+
 // Complete Code
 public class SecondActivity {
 
 	public static void main(String[] args) {
-		SecondActivity calculator = new SecondActivity();
+		Calculator calculator = new Calculator();
 
+		Scanner reader = new Scanner(System.in);
+		double number1 = reader.nextDouble();
+		double number2 = reader.nextDouble();
+		reader.close();
 
 		//TODO Use the calculator to calculate different values
 		//Example ->
 		//calculator.sum(10.2, 5);
 		//calculator.divide(100, 33);
 		// and more
+
+        System.out.println("The result for sum is " + calculator.sum(number1, number2));
+		System.out.println("The result for division is " + calculator.division(number1,number2));
+		System.out.println("The result multiplication is " + calculator.multiplication(number1, number2));
+		System.out.println("The result difference is " + calculator.difference(number1, number2));
 
 	}
 
