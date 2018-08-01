914591bf11c29399b028ff0d4da530e842d2ab56
Student <ghjh>
2018-08-01 16:57:05 +0300

    Class Test Work

--- SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
+++ SEF/src/sef/ATestTask/ThirdActivity/ThirdActivity.java
@@ -8,8 +8,11 @@ public class ThirdActivity {
     public static void main(String[] args) throws Exception {
         ThirdActivity ta = new ThirdActivity();
         //TODO handle exception on proper way
-        ta.validateUser("Ivan");
+
         ta.catchExeption();
+        ta.catchMe(1,0);
+
+        ta.validateUser("Ivan");
     }
 
     void catchExeption() {
@@ -19,26 +22,33 @@ public class ThirdActivity {
                 System.out.println(arr[i]);
             }
             System.out.println("This should get printed even if there is an exception");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Exception occured");
         }
+    }
 
-    void validateUser(String String name;
-        name) throws Exception {
+    void validateUser(String name) throws Exception {
         String[] validUsers = {"John", "Mike", "Missi", "Peacy"};
         boolean flag = false;
-        for(int i=0;i<validUsers.length;i++){
-            if(validUsers[i].equals(name)){
-        }
-        //TODO if name in a list -> set flag=1
-        // if at the end flag=0 -> throw the exeption
-        if(flag== false) {
-            throw new Exception("User is not valid");
+        for (int i = 0; i < validUsers.length; i++) {
+            if (validUsers[i].equals(name)) {
+                System.out.println("User is valid");
+            }
+            else{
+                System.out.println("Invalid");
+            }
+            //TODO if name in a list -> set flag=1
+            // if at the end flag=0 -> throw the exeption
+            if (flag == false) {
+                throw new Exception("User is not valid");
+            }
         }
     }
 
     void catchMe(int num1, int num2)
-    {
+        {
+            num1=10;
+            num2= 4;
         //TODO Catch exeption
         try {
             int result = num1 / num2;
