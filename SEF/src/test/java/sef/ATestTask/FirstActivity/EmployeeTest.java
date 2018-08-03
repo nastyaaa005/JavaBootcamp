bb49db90e42b57a6a0b8f003c3b7f628ab946198
Solvita <solvita.v@github.com>
2018-08-03 21:50:40 +0300

    fixes

--- SEF/src/test/java/sef/ATestTask/FirstActivity/EmployeeTest.java
+++ SEF/src/test/java/sef/ATestTask/FirstActivity/EmployeeTest.java
@@ -9,9 +9,9 @@ public class EmployeeTest {
     //Example
     @Test
     public void checkEmployeeCreatedProperly(){
-//        Employee employee = new Employee();
-//        employee.setFirstName("Test1");
-//        assertEquals("I am an error message", "Test1", employee.getFirstName());
+        Employee employee = new Employee("first", "second",  4, 4, "title", "company", 4);
+        employee.setFirstName("Test1");
+        assertEquals("I am an error message", "Test1", employee.getFirstName());
     }
 
 }
--- SEF/src/test/java/sef/ATestTask/SecondActivity/SecondActivityTest.java
+++ SEF/src/test/java/sef/ATestTask/SecondActivity/SecondActivityTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 
 public class SecondActivityTest extends TestCase {
 
-	private SecondActivity theCalculator;
+	private Calculator theCalculator;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		//	Initialize variables to be used here
-		theCalculator = new SecondActivity();
+		theCalculator = new Calculator();
 	}
 
 	protected void tearDown() throws Exception {
@@ -20,15 +20,15 @@ public class SecondActivityTest extends TestCase {
 	//Example ->
 	@Test
 	public void testGetSumPositiveValue() {
-//		assertEquals(15, (theCalculator.getSum(5, 10)));
-//		assertEquals(15, (theCalculator.getSum(10, 5)));
+		assertEquals(15.0, (theCalculator.sum(5, 10)));
+		assertEquals(15.0, (theCalculator.sum(10, 5)));
 	}
 
 	//Example ->
 	@Test
 	public void testGetSumNegativeValue() {
-//		assertEquals(5, (theCalculator.getSum(-5, 10)));
-//		assertEquals(-5, (theCalculator.getSum(5, -10)));
+		assertEquals(5.0, (theCalculator.sum(-5, 10)));
+		assertEquals(-5.0, (theCalculator.sum(5, -10)));
 	}
 
 }
